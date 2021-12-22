4981cb308a149198fd2148dc71ebbd4365f6237a
Kristian <13659334+codecker@users.noreply.github.com>
2021-12-22 09:56:46 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,12 @@ terraform {
   # Define required Terraform version
   required_version = "1.0.11"
 }
+
+resource "aws_vpc" "actions_vpc" {
+   cidr_block       = "10.10.0.0/16"
+   instance_tenancy = "default"
+
+  tags = {
+    Name = "action-vpc"
+  }
+}
